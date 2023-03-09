dc02b1d12c29177979d9bc8d813b20989fbe04b2
unknown <ahassan2001@msn.com>
2023-03-09 16:05:56 -0500

    first push

--- public/index.js
+++ public/index.js
@@ -4,6 +4,37 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    fetch('https://api.twelvedata.com/api_usage?apikey=af21ef289e9f4495a624b005684820d8')
+    
+    stocks.forEach( stock => stock.values.reverse())
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: stocks[0].values.reverse().map(value => value.datetime),
+            datasets: stocks.map(stock => ({
+                label: stock.meta.symbol,
+                data: stock.values.reverse().map(value => parseFloat(value.high)),
+                backgroundColor: getColor(stock.meta.symbol),
+                borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
 }
 
 main()
